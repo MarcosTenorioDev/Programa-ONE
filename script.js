caec46620fd4428cde6fe6e2fcee1da4eaae694d
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-04-18 09:40:08 -0300

    :sparkles: feat: add function do decrypt code

--- script.js
+++ script.js
@@ -6,39 +6,60 @@ let imageNotFound = document.getElementById('imageNotFound');
 let labelImage = document.getElementById('labelImage');
 let imageDescription = document.getElementById('imageDescription');
 let result = document.getElementById('result');
+let codeDecrypted = '';
 
 
 //Função main
-buttonEncrypt.onclick = () =>{
+buttonEncrypt.onclick = () => {
     codeToEncrypt = inputText.value; //código para ser criptografado (input)
     encrypt(codeToEncrypt); //processo de criptogração (função)
     codeEncrypted = encrypt(codeToEncrypt); //codigo criptografado (output)
     hideRightSideElements(); //Função para esconder os elementos estáticos do outrput
     showCodeEncrypt(codeEncrypted); //Função para mostrar o código criptografado no HTML
-    
+}
 
+//Função traduzir criptografia
+buttonDecrypt.onclick = () =>{
+    decrypt(codeEncrypted);
+    showCodeDecrypted(codeDecrypted);
 }
 
 //função para encriptar os dados
-const encrypt = (codeToEncrypt) =>{
+const encrypt = (codeToEncrypt) => {
 
     codeToEncrypt = codeToEncrypt
-    .replaceAll('e', 'enter')
-    .replaceAll('i', 'imes')
-    .replaceAll('o', 'ober')
-    .replaceAll('a', 'ai')
-    .replaceAll('u','ufat');
-    
+        .replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('o', 'ober')
+        .replaceAll('a', 'ai')
+        .replaceAll('u', 'ufat');
+
     return codeToEncrypt
 
 }
 
+//função para descriptografar os dados
+const decrypt = (codeEncrypted) =>{
+    codeDecrypted = codeEncrypted
+    .replaceAll('enter', 'e')
+    .replaceAll('imes','i')
+    .replaceAll('ober','o')
+    .replaceAll('ai','a')
+    .replaceAll('ufat','u');
+
+    return codeDecrypted;
+}
+
 //Função para mostrar o código criptografado no HTML
-const showCodeEncrypt = (codeEncrypted) =>{
-    result.innerHTML = `${codeEncrypted}`
+const showCodeEncrypt = (codeEncrypted) => {
+    result.innerHTML = `${codeEncrypted}`;
+}
+//Função para mostrar o código descriptografado
+const showCodeDecrypted = (codeDecrypted) =>{
+    result.innerHTML = `${codeDecrypted}`;
 }
 //Função para esconder os elementos estáticos do output
-const hideRightSideElements = () =>{
+const hideRightSideElements = () => {
     imageNotFound = imageNotFound.classList.add('hide');
     labelImage = labelImage.classList.add('hide');
     imageDescription = imageDescription.classList.add('hide');
